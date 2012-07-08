5997b3c0f505e41ba645c5203faf46bfcff191d8
Samuel Martin <s.martin49@gmail.com>
2012-07-08 16:02:48 +0200

    pkg-utils.mk: fix indentation

--- package/pkg-utils.mk
+++ package/pkg-utils.mk
@@ -16,12 +16,14 @@
 [FROM] := a b c d e f g h i j k l m n o p q r s t u v w x y z . -
 [TO]   := A B C D E F G H I J K L M N O P Q R S T U V W X Y Z _ _
 
-UPPERCASE = $(strip $(eval __tmp := $1) \
-     $(foreach c, $(join $(addsuffix :,$([FROM])),$([TO])), \
-	$(eval __tmp :=	\
-		$(subst $(word 1,$(subst :, ,$c)),$(word 2,$(subst :, ,$c)),\
-			$(__tmp)))) \
-     $(__tmp))
+UPPERCASE = \
+	$(strip $(eval __tmp := $1) \
+		$(foreach c, $(join $(addsuffix :,$([FROM])),$([TO])), \
+			$(eval __tmp :=	\
+				$(subst $(word 1,$(subst :, ,$c)),\
+					$(word 2,$(subst :, ,$c)),\
+					$(__tmp)))) \
+		$(__tmp))
 
 #
 # Manipulation of .config files based on the Kconfig
@@ -30,18 +32,18 @@ UPPERCASE = $(strip $(eval __tmp := $1) \
 #
 
 define KCONFIG_ENABLE_OPT
-       $(SED) "/\\<$(1)\\>/d" $(2)
-       echo "$(1)=y" >> $(2)
+	$(SED) "/\\<$(1)\\>/d" $(2)
+	echo "$(1)=y" >> $(2)
 endef
 
 define KCONFIG_SET_OPT
-       $(SED) "/\\<$(1)\\>/d" $(3)
-       echo "$(1)=$(2)" >> $(3)
+	$(SED) "/\\<$(1)\\>/d" $(3)
+	echo "$(1)=$(2)" >> $(3)
 endef
 
 define KCONFIG_DISABLE_OPT
-       $(SED) "/\\<$(1)\\>/d" $(2)
-       echo "# $(1) is not set" >> $(2)
+	$(SED) "/\\<$(1)\\>/d" $(2)
+	echo "# $(1) is not set" >> $(2)
 endef
 
 # Helper functions to determine the name of a package and its
@@ -51,15 +53,15 @@ endef
 # pkgdir macro is carefully written to handle the case of the Linux
 # package, for which the package directory is an empty string.
 define pkgdir
-$(dir $(lastword $(MAKEFILE_LIST)))
+	$(dir $(lastword $(MAKEFILE_LIST)))
 endef
 
 define pkgname
-$(lastword $(subst /, ,$(call pkgdir)))
+	$(lastword $(subst /, ,$(call pkgdir)))
 endef
 
 define pkgparentdir
-$(patsubst %$(call pkgname)/,%,$(call pkgdir))
+	$(patsubst %$(call pkgname)/,%,$(call pkgdir))
 endef
 
 # Define extractors for different archive suffixes
@@ -72,15 +74,15 @@ INFLATE.xz   = $(XZCAT)
 INFLATE.tar  = cat
 
 # MESSAGE Macro -- display a message in bold type
-MESSAGE = echo "$(TERM_BOLD)>>> $($(PKG)_NAME) $($(PKG)_VERSION) $(1)$(TERM_RESET)"
-TERM_BOLD := $(shell tput smso)
+MESSAGE      = echo "$(TERM_BOLD)>>> $($(PKG)_NAME) $($(PKG)_VERSION) $(1)$(TERM_RESET)"
+TERM_BOLD  := $(shell tput smso)
 TERM_RESET := $(shell tput rmso)
 
 # Utility functions for 'find'
 # findfileclauses(filelist) => -name 'X' -o -name 'Y'
 findfileclauses = $(call notfirstword,$(patsubst %,-o -name '%',$(1)))
 # finddirclauses(base, dirlist) => -path 'base/dirX' -o -path 'base/dirY'
-finddirclauses = $(call notfirstword,$(patsubst %,-o -path '$(1)/%',$(2)))
+finddirclauses  = $(call notfirstword,$(patsubst %,-o -path '$(1)/%',$(2)))
 
 # Miscellaneous utility functions
 # notfirstword(wordlist): returns all but the first word in wordlist
