c9d4104f042ff224f797fa865c373404f685deaf
Samuel Martin <s.martin49@gmail.com>
2012-07-08 19:52:20 +0200

    automake: disable rpath fix step
    Automake only install perl scripts, so in this case, fixing rpath is
    unneeded.

--- package/automake/automake.mk
+++ package/automake/automake.mk
@@ -13,6 +13,8 @@ AUTOMAKE_DEPENDENCIES = host-autoconf autoconf perl
 
 HOST_AUTOMAKE_DEPENDENCIES = host-autoconf
 
+HOST_AUTOMAKE_FIX_RPATH = NO
+
 define GTK_DOC_M4_INSTALL
  $(INSTALL) -D -m 0644 package/automake/gtk-doc.m4 $(HOST_DIR)/usr/share/aclocal/gtk-doc.m4
 endef
