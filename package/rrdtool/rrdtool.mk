0d9a2414cfbb891f628ce5c9bb1e979dd3f9c8ec
Samuel Martin <s.martin49@gmail.com>
2012-11-17 13:19:03 +0100

    rrdtool: add python binding support

--- package/rrdtool/rrdtool.mk
+++ package/rrdtool/rrdtool.mk
@@ -14,9 +14,25 @@ RRDTOOL_AUTORECONF = YES
 RRDTOOL_INSTALL_STAGING = YES
 RRDTOOL_CONF_ENV = rd_cv_ieee_works=yes rd_cv_null_realloc=nope \
 			ac_cv_func_mmap_fixed_mapped=yes
-RRDTOOL_CONF_OPT = --disable-perl --disable-python --disable-ruby \
-			--disable-tcl --program-transform-name='' \
+
+RRDTOOL_CONF_OPT = --program-transform-name='' \
+			--disable-perl \
+			--disable-ruby \
+			--disable-tcl \
 			$(if $(BR2_TOOLCHAIN_HAS_THREADS),,--disable-pthread)
+
+ifneq ($(BR2_PACKAGE_RRDTOOL_PYTHON),)
+RRDTOOL_CONF_OPT += --enable-python
+RRDTOOL_CONF_ENV += \
+	am_cv_pathless_PYTHON=python \
+	ac_cv_path_PYTHON=$(HOST_DIR)/usr/bin/python \
+	am_cv_python_platform=linux2 \
+	am_cv_python_includes=$(STAGING_DIR)/usr/include/python$(PYTHON_VERSION_MAJOR)
+RRDTOOL_DEPENDENCIES += python
+else
+RRDTOOL_CONF_OPT += --disable-python
+endif
+
 RRDTOOL_MAKE = $(MAKE1)
 
 define RRDTOOL_REMOVE_EXAMPLES
