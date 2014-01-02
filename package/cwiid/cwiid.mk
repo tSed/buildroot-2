500709a9b740af51290659213abc2b5f39ea261a
Samuel Martin <s.martin49@gmail.com>
2014-01-02 13:40:10 +0100

    cwiid: new package
    This patch only add the native cwiid library and a couple of tools.

--- package/cwiid/cwiid.mk
+++ package/cwiid/cwiid.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-CWIID_VERSION = 0.6.00
-CWIID_SOURCE = cwiid-$(CWIID_VERSION).tgz
-CWIID_SITE = http://abstrakraft.org/cwiid/downloads
+CWIID_VERSION = c6583fd
+#CWIID_VERSION = 0.6.00
+#CWIID_SOURCE = cwiid-$(CWIID_VERSION).tgz
+CWIID_SITE = $(call github,tsed,cwiid,$(CWIID_VERSION))
+#CWIID_SITE = http://abstrakraft.org/cwiid/downloads
 CWIID_LICENSE = GPLv2+
 CWIID_LICENSE_FILES = COPYING
 
@@ -15,10 +17,11 @@ CWIID_INSTALL_STAGING = YES
 
 CWIID_DEPENDENCIES = host-pkgconf host-bison host-flex bluez_utils
 
+# Disable ldconfig
 # Disable python support. This disables the 2 following things:
 #   - wminput Python plugin support
 #   - cwiid Python module
-CWIID_CONF_OPT = --without-python --disable-ldconfig
+CWIID_CONF_OPT = --disable-ldconfig --without-python
 
 ifeq ($(BR2_PACKAGE_CWIID_WMGUI),y)
 CWIID_DEPENDENCIES += libgtk2 libglib2
