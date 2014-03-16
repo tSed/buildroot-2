e046c00c1e6c398081968aaf15f6bb65c7c3cfd0
Samuel Martin <s.martin49@gmail.com>
2014-03-16 14:25:37 +0100

    octomap: add dynamicEDT3D support
    Because dynamicEDT3D depends on octomap, and needs octomap to be installed
    prior to be built, then add it as a new package.

--- package/octomap/octomap.mk
+++ package/octomap/octomap.mk
@@ -29,3 +29,28 @@ OCTOMAP_POST_INSTALL_TARGET_HOOKS += OCTOMAP_TARGET_INSTALL_REMOVE_PROGRAMS
 endif
 
 $(eval $(cmake-package))
+
+#
+# octomap source tree includes source code for 3 projects:
+# - octomap, under the octomap subdirectory;
+# - dynamic Euclidian distance, under the dynamicEDT3D subdirectory;
+# - octovis, under the octovis subdirectory.
+# Unfortunately, the last 2 sub-projects depend on octomap. This means that
+# octomap needs to be installed prior to be able build them.
+# So, add them as independent packages
+#
+# octovis is not added since it depends on packages not (yet) supported in
+# Buildroot (qglviewer).
+#
+
+DYNAMICEDT3D_VERSION = $(OCTOMAP_VERSION)
+DYNAMICEDT3D_LICENSE = $(OCTOMAP_LICENSE)
+DYNAMICEDT3D_LICENSE_FILES = $(OCTOMAP_LICENSE_FILES)
+DYNAMICEDT3D_SOURCE = $(OCTOMAP_SOURCE)
+DYNAMICEDT3D_SITE = $(OCTOMAP_SITE)
+DYNAMICEDT3D_INSTALL_STAGING = YES
+DYNAMICEDT3D_SUBDIR = dynamicEDT3D
+DYNAMICEDT3D_DEPENDENCIES = octomap
+DYMANICEDT3D_CONF_OPT = $(OCTOMAP_CONF_OPT)
+
+$(eval $(call inner-cmake-package,dynamicedt3d,DYNAMICEDT3D,DYNAMICEDT3D,target))
