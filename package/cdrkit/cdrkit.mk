eec2ae3584e1c75d20bf01980a3184f339f2c403
Samuel Martin <s.martin49@gmail.com>
2014-08-15 16:05:08 +0200

    package/cdrkit: clean {HOST_,}CDRKIT_CONF_OPT
    Extending {C,LD}FLAGS for both host and target cdrkit package is not needed
    because it is already handled by the {host-,}cmake-package infrastructure.
    
    Signed-off-by: Samuel Martin <s.martin49@gmail.com>

--- package/cdrkit/cdrkit.mk
+++ package/cdrkit/cdrkit.mk
@@ -18,12 +18,5 @@ endif
 
 CDRKIT_CONF_OPT += $(CMAKE_ENDIAN_OPT)
 
-## cdrkit isn't completely re-rooted by CMAKE_FIND_ROOT_PATH, so add
-## some extra flags so it finds needed libs and headers.
-CDRKIT_CONF_OPT += -DCMAKE_C_FLAGS="-I$(STAGING_DIR)/usr/include"
-CDRKIT_CONF_OPT += -DCMAKE_EXE_LINKER_FLAGS="$(TARGET_LDFLAGS)"
-HOST_CDRKIT_CONF_OPT += -DCMAKE_C_FLAGS="-I$(HOST_DIR)/usr/include"
-HOST_CDRKIT_CONF_OPT += -DCMAKE_EXE_LINKER_FLAGS="$(HOST_LDFLAGS)"
-
 $(eval $(cmake-package))
 $(eval $(host-cmake-package))
